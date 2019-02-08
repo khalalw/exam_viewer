c8810170ae455c036e8a28d082ac1321304d669b
Khalal Walker <khalal.walker@gmail.com>
2019-02-08 15:43:24 -0800

    securing flask with authO

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask import Flask, jsonify, request
 from .entities.entity import Session, engine, Base
 from .entities.exam import Exam, ExamSchema
+from .auth import AuthError, requires_auth
 
 # creating the Flask application
 app = Flask(__name__)
@@ -44,3 +45,10 @@ def add_exam():
     new_exam = ExamSchema().dump(exam).data
     session.close()
     return jsonify(new_exam), 201
+
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
