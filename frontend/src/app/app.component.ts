e5105cc466a905e190166b949d23683f2f0a427a
Khalal Walker <khalal.walker@gmail.com>
2019-02-07 17:39:52 -0800

    integrating flask and angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ExamsApiService } from './exams/exams-api.service';
+import { Exam } from './exams/exam.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'app';
+  examsListSubs: Subscription;
+  examsList: Exam[];
+
+  constructor(private examsApi: ExamsApiService) {}
+
+  ngOnInit(): void {
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
+    this.examsListSubs = this.examsApi.getExams().subscribe(res => {
+      this.examsList = res;
+    }, console.error);
+  }
+
+  ngOnDestroy(): void {
+    // Called once, before the instance is destroyed.
+    // Add 'implements OnDestroy' to the class.
+    this.examsListSubs.unsubscribe();
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ExamsApiService } from './exams/exams-api.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [ExamsApiService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
